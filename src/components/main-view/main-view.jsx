c47b32a0a231b2b56d35362886c9a0c7075af139
cutraniramlan <cutraniramlan@gmail.com>
2022-12-15 09:30:35 +0100

    update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -50,8 +50,8 @@ export const MainView = () => {
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
-          book={movie}
-          onBookClick={(newSelectedMovie) => {
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
         />
