24d880936f47d9eceb1e3ef046b9ed5cb63d319d
cutraniramlan <cutraniramlan@gmail.com>
2022-12-28 19:59:51 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./index.scss";
+$primary: SeaGreen;
+$body-bg: Honeydew;
+
+@import '~bootstrap/scss/bootstrap.scss';
 
 const App = () => {
   return <MainView />;
