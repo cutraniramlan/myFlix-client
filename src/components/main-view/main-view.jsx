9a0610a8f9c2a887ae7244d430085dd0824b4e73
cutraniramlan <cutraniramlan@gmail.com>
2022-12-28 11:25:22 +0100

    update main view & signup view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,18 +1,104 @@
+// import { useState, useEffect } from "react";
+// import { MovieCard } from "../movie-card/movie-card";
+// import { MovieView } from "../movie-view/movie-view";
+// import { LoginView } from "../login-view/login-view";
+// import { SignupView } from "../signup-view/signup-view";
+// import Container  from 'react-bootstrap/Container';
+// import Row from "react-bootstrap/Row";
+
+
+// export const MainView = () => {
+//     const storedUser = JSON.parse(localStorage.getItem("user"));
+//     const storedToken = localStorage.getItem("token");
+//     const [user, setUser] = useState(storedUser? storedUser : null);
+//     const [token, setToken] = useState(storedToken? storedToken : null);
+//     const [movies, setMovies] = useState([]);
+//     const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  
+//    useEffect(() => {
+//      if (!token) return;
+  
+//      fetch("https://myflixmoviedb.herokuapp.com/movies", {
+//        headers: { Authorization: `Bearer ${token}` },
+//      })
+//        .then((response) => response.json())
+//        .then((movies) => {
+//          setMovies(movies);
+  
+//        });
+//    }, [token]);
+  
+//     // rest of the code
+
+//   <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+
+//   /* if (!user) {
+//     return <LoginView onLoggedIn={(user) => setUser(user)} />;
+//   } */
+
+//   if (!user) {
+//     return (
+//       <>
+//         <LoginView onLoggedIn={(user, token) => {
+//           setUser(user);
+//           setToken(token);
+//         }} />
+//         or
+//         <SignupView />
+//       </>
+//     );
+//   }
+
+//    if (selectedMovie) {
+//     return (
+//       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+//     );
+//   }
+
+//   if (movies.length === 0) {
+//     return <div>The list is empty!</div>;
+//   } 
+
+//   if (data.user) {
+//     localStorage.setItem("user", JSON.stringify(data.user));
+//     localStorage.setItem("token", data.token);
+//     onLoggedIn(data.user, data.token);
+//   } else {
+//     alert("No such user");
+//   }
+
+//   return (
+//     <div>
+//       {movies.map((movie) => (
+//         <MovieCard
+//           key={movie.id}
+//           movie={movie}
+//           onMovieClick={(newSelectedMovie) => {
+//             setSelectedMovie(newSelectedMovie);
+//           }}
+//         />
+//       ))}
+//     </div>
+//   );
+
+//   return (
+//     <Container style={{border: "1px solid red"}}>
+//       <MainView />
+//     </Container>
+//   );
+// }
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import Row from "react-bootstrap/Row";
 
-
-/* export const MainView = () => {
+export const MainView = () => {
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
-
   const [user, setUser] = useState(null);
-
-  const [token, setToken] = useState(null);
 
   useEffect(() => {
     fetch("https://openlibrary.org/search.json?q=star+wars")
@@ -23,7 +109,7 @@ import { SignupView } from "../signup-view/signup-view";
             id: doc.key,
             title: doc.title,
             image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
-            director: doc.director_name?.[0]
+            author: doc.author_name?.[0]
           };
         });
 
@@ -31,92 +117,34 @@ import { SignupView } from "../signup-view/signup-view";
       });
   }, []);
 
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
-
-    fetch("https://myflixmoviedb.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, [token]); */
-
-  export const MainView = () => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedToken = localStorage.getItem("token");
-    const [user, setUser] = useState(storedUser? storedUser : null);
-    const [token, setToken] = useState(storedToken? storedToken : null);
-    const [movies, setMovies] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null);
-  
-  
-   useEffect(() => {
-     if (!token) return;
-  
-     fetch("https://myflixmoviedb.herokuapp.com/movies", {
-       headers: { Authorization: `Bearer ${token}` },
-     })
-       .then((response) => response.json())
-       .then((movies) => {
-         setMovies(movies);
-  
-       });
-   }, [token]);
-  
-    // rest of the code
-
-  <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
-
-  /* if (!user) {
-    return <LoginView onLoggedIn={(user) => setUser(user)} />;
-  } */
-
-  if (!user) {
-    return (
-      <>
-        <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
-        or
-        <SignupView />
-      </>
-    );
-  }
-
-   if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  } 
-
-  if (data.user) {
-    localStorage.setItem("user", JSON.stringify(data.user));
-    localStorage.setItem("token", data.token);
-    onLoggedIn(data.user, data.token);
-  } else {
-    alert("No such user");
-  }
-
   return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
+      <Row> 
+        {!user ? (
+          <>
+            <LoginView onLoggedIn={(user) => setUser(user)} />
+            or
+            <SignupView />
+          </>
+        ) : selectedMovie ? (
+          <MovieView 
+            movie={selectedMovie} 
+            onBackClick={() => setSelectedMovie(null)} 
+          />
+        ) : movies.length === 0 ? (
+          <div>The list is empty!</div>
+        ) : (
+          <>
+            {movies.map((movie) => (
+              <MovieCard
+                key={movie.id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            ))}
+          </>
+        )}
+      </Row>
   );
-}
+};
--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,131 +1,132 @@
-import { useState } from "react";
+import{ useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const SignupView = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [birthday, setBirthday] = useState("");
-  
-    const handleSubmit = (event) => {};
-    // Same code...
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
-    return (
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            minLength="3" 
-          />
-        </Form.Group>
-  
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
+  const handleSubmit = (event) => {};
 
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email:</Form.Label>
-          <Form.Control
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            minLength="3" 
-          />
-        </Form.Group>
+  /* return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}; */
+return (
+  <Form onSubmit={handleSubmit}>
+    <Form.Group controlId="formUsername">
+      <Form.Label>Username:</Form.Label>
+      <Form.Control
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+        minLength="3" 
+      />
+    </Form.Group>
 
-        <Form.Group controlId="formBirthday">
-          <Form.Label>Birthday:</Form.Label>
-          <Form.Control
-            type="text"
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-            required
-            minLength="3" 
-          />
-        </Form.Group>
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password:</Form.Label>
+      <Form.Control
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+    </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    );
+    <Form.Group controlId="formEmail">
+      <Form.Label>Email:</Form.Label>
+      <Form.Control
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        minLength="3" 
+      />
+    </Form.Group>
+
+    <Form.Group controlId="formBirthday">
+      <Form.Label>Birthday:</Form.Label>
+      <Form.Control
+        type="date"
+        value={birthday}
+        onChange={(e) => setBirthday(e.target.value)}
+        required
+      />
+    </Form.Group>
+
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const data = {
+    Username: username,
+    Password: password,
+    Email: email,
+    Birthday: birthday
   };
 
-/*     return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            minLength="3"
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Birthday:
-          <input
-            type="date"
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }; */
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    };
-
-    fetch("https://movie-api-rani-1.herokuapp.com/", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then((response) => {
-      if (response.ok) {
-        alert("Signup successful");
-        window.location.reload();
-      } else {
-        alert("Signup failed");
-      }
-    });
-  };
+  fetch("SIGNUP_URL", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then((response) => {
+    if (response.ok) {
+      alert("Signup successful");
+      window.location.reload();
+    } else {
+      alert("Signup failed");
+    }
+  });
+};
