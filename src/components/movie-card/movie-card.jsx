78c41e5749e48722d20fe93214a537ffe4357cf5
cutraniramlan <cutraniramlan@gmail.com>
2023-01-01 14:35:56 +0100

    delete onMovieClick Proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,13 @@
-import React            from "react";
+import React from "react";
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
@@ -25,5 +27,4 @@ MovieCard.propTypes = {
     image: PropTypes.string.isRequired,
     director: PropTypes.string
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
