75a9c26ab6ce719b42fd1193ef671526f5844014
cutraniramlan <cutraniramlan@gmail.com>
2023-01-07 10:09:48 +0100

    change movie to movies

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import './movie-view.scss';
 
-export const MovieView = ({ movie}) => {
+export const MovieView = ({ movies}) => {
 
   const { movieId } = useParams();
 
-  const movie = movie.find((b) => b.id === movieId);
+  const movie = movies.find((b) => b.id === movieId);
 
     return (
       <div>
